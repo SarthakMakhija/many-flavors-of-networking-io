bf1c9fe0562811a997648ffb517dea69fe55ff81
sarthak <sarthak.makhija@gmail.com>
2024-05-19 17:45:52 +0530

    Sarthak | Adds an implementation of non blocking busy wait

---
+++ non_blocking_busy_waiting/server.go
@@ -0,0 +1,90 @@
+package non_blocking_busy_waiting
+
+import (
+	"errors"
+	"log"
+	"many-flavors-of-nwing-io/non_blocking_busy_waiting/conn"
+	"many-flavors-of-nwing-io/non_blocking_busy_waiting/proto"
+	store2 "many-flavors-of-nwing-io/non_blocking_busy_waiting/store"
+	"net"
+	"syscall"
+)
+
+const MaxClients = 10_000
+
+// TCPServer represents an async TCP TCPServer
+type TCPServer struct {
+	serverFd    int
+	handlers    map[uint32]conn.Handler
+	stopChannel chan struct{}
+}
+
+// NewTCPServer creates a new instance of TCPServer.
+func NewTCPServer(host string, port uint16) (*TCPServer, error) {
+	//starts the listener on the given port and returns the server file descriptor, if there is no error.
+	startListener := func() (int, error) {
+		serverFd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+		if err != nil {
+			_ = syscall.Close(serverFd)
+			return -1, err
+		}
+		if err = syscall.SetNonblock(serverFd, true); err != nil {
+			_ = syscall.Close(serverFd)
+			return -1, err
+		}
+
+		ip4 := net.ParseIP(host)
+		if err = syscall.Bind(serverFd, &syscall.SockaddrInet4{
+			Port: int(port),
+			Addr: [4]byte{ip4[0], ip4[1], ip4[2], ip4[3]},
+		}); err != nil {
+			_ = syscall.Close(serverFd)
+			return -1, err
+		}
+		if err = syscall.Listen(serverFd, MaxClients); err != nil {
+			return -1, err
+		}
+		return serverFd, nil
+	}
+	serverFd, err := startListener()
+	if err != nil {
+		return nil, err
+	}
+
+	store := store2.NewInMemoryStore()
+	return &TCPServer{
+		serverFd: serverFd,
+		handlers: map[uint32]conn.Handler{
+			proto.KeyValueMessageKindPutOrUpdate: conn.NewPutOrUpdateHandler(store),
+			proto.KeyValueMessageKindGet:         conn.NewGetHandler(store),
+		},
+		stopChannel: make(chan struct{}),
+	}, nil
+}
+
+// Start starts the server.
+func (server *TCPServer) Start() {
+	for {
+		select {
+		case <-server.stopChannel:
+			return
+		default:
+			connectionFd, _, err := syscall.Accept(server.serverFd)
+			if err != nil {
+				if errors.Is(err, syscall.EAGAIN) || errors.Is(err, syscall.EWOULDBLOCK) {
+					continue
+				}
+				return
+			}
+			_ = syscall.SetNonblock(connectionFd, true)
+			conn.NewClient(connectionFd, server.handlers).Run()
+		}
+	}
+}
+
+// Stop stops the server.
+func (server *TCPServer) Stop() {
+	log.Println("Stopping TCPServer")
+	_ = syscall.Close(server.serverFd)
+	close(server.stopChannel)
+}
---
+++ non_blocking_busy_waiting/server_test.go
@@ -0,0 +1,100 @@
+package non_blocking_busy_waiting
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"many-flavors-of-nwing-io/non_blocking_busy_waiting/conn"
+	"many-flavors-of-nwing-io/non_blocking_busy_waiting/proto"
+	"math/rand"
+	"net"
+	"testing"
+)
+
+func TestSendsAPutOrUpdateAndGetOverAConnection(t *testing.T) {
+	port := randomPort()
+	server, err := NewTCPServer("127.0.0.1", port)
+	assert.Nil(t, err)
+
+	go func() {
+		server.Start()
+	}()
+
+	defer func() {
+		server.Stop()
+	}()
+
+	connection, err := net.Dial("tcp", fmt.Sprintf("%v:%v", "127.0.0.1", port))
+	assert.Nil(t, err)
+
+	buffer, _ := proto.NewPutOrUpdateKeyValueMessage("DiskType", "NVMe SSD").Serialize()
+	_, _ = connection.Write(buffer)
+
+	buffer, _ = proto.NewPutOrUpdateKeyValueMessage("KV", "Distributed").Serialize()
+	_, _ = connection.Write(buffer)
+
+	buffer, _ = proto.NewGetValueMessage("KV").Serialize()
+	_, _ = connection.Write(buffer)
+
+	connectionReader := conn.NewConnectionReader(connection)
+	_, _ = connectionReader.AttemptReadOrErrorOut()
+	_, _ = connectionReader.AttemptReadOrErrorOut()
+
+	message, err := connectionReader.AttemptReadOrErrorOut()
+
+	assert.Nil(t, err)
+	assert.Equal(t, "Distributed", message.Value)
+}
+
+func TestSendsMultiplePutOrUpdateAndAGetOverAConnection(t *testing.T) {
+	port := randomPort()
+	server, err := NewTCPServer("127.0.0.1", port)
+	assert.Nil(t, err)
+
+	go func() {
+		server.Start()
+	}()
+
+	defer func() {
+		server.Stop()
+	}()
+
+	connection, err := net.Dial("tcp", fmt.Sprintf("%v:%v", "127.0.0.1", port))
+	assert.Nil(t, err)
+
+	sendMultiplePutOrUpdates := func() {
+		buffer, _ := proto.NewPutOrUpdateKeyValueMessage("DiskType", "NVMe SSD").Serialize()
+		_, _ = connection.Write(buffer)
+
+		buffer, _ = proto.NewPutOrUpdateKeyValueMessage("Storage", "LSM").Serialize()
+		_, _ = connection.Write(buffer)
+
+		buffer, _ = proto.NewPutOrUpdateKeyValueMessage("System", "Distributed").Serialize()
+		_, _ = connection.Write(buffer)
+
+		buffer, _ = proto.NewGetValueMessage("System").Serialize()
+		_, _ = connection.Write(buffer)
+	}
+	attemptLastRead := func() (*proto.KeyValueMessage, error) {
+		connectionReader := conn.NewConnectionReader(connection)
+		_, _ = connectionReader.AttemptReadOrErrorOut()
+		_, _ = connectionReader.AttemptReadOrErrorOut()
+		_, _ = connectionReader.AttemptReadOrErrorOut()
+
+		return connectionReader.AttemptReadOrErrorOut()
+	}
+
+	sendMultiplePutOrUpdates()
+	//time.Sleep(100 * time.Second)
+	message, err := attemptLastRead()
+
+	assert.Nil(t, err)
+	assert.Equal(t, "Distributed", message.Value)
+}
+
+func randomPort() uint16 {
+	port := 0
+	for port = rand.Intn(10000); port < 2000; port = rand.Intn(10000) {
+		continue
+	}
+	return uint16(port)
+}
